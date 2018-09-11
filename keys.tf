749c894d707bb5b03385b74755e6b66abcd3eddc
Matthias Malzahn <matthias.malzahn@comlineag.de>
2018-09-11 15:30:27 +0200

    add optional Keygeneration

--- keys.tf
+++ keys.tf
@@ -1,33 +1,40 @@
 resource "tls_private_key" "private_key_dockercluster" {
+  count = "${var.aws_key_name == "" ? 1 : 0}"
   algorithm = "RSA"
   rsa_bits = 4096
 }
 
 resource "local_file" "privateKeyFile" {
+  count = "${var.aws_key_name == "" ? 1 : 0}"
   content  = "${tls_private_key.private_key_dockercluster.private_key_pem}"
   filename = "${path.module}/keys/${terraform.workspace}/${lower(random_string.projectId.result)}.key.pem"
 }
 
 resource "local_file" "publicKeyFile" {
+  count = "${var.aws_key_name == "" ? 1 : 0}"
   content  = "${tls_private_key.private_key_dockercluster.public_key_pem}"
   filename = "${path.module}/keys/${terraform.workspace}/${lower(random_string.projectId.result)}.pem"
 }
 resource "local_file" "privateKeyFile_docker" {
+  count = "${var.aws_key_name == "" ? 1 : 0}"
   content  = "${tls_private_key.private_key_dockercluster.private_key_pem}"
   filename = "${path.module}/keys/${terraform.workspace}/docker/key.pem"
 }
 
 resource "local_file" "publicKeyFile_docker" {
+  count = "${var.aws_key_name == "" ? 1 : 0}"
   content  = "${tls_private_key.private_key_dockercluster.public_key_pem}"
   filename = "${path.module}/keys/${terraform.workspace}/docker/cert.pem"
 }
 
 resource "local_file" "publicKeyFileOpenSsh" {
+  count = "${var.aws_key_name == "" ? 1 : 0}"
   content  = "${tls_private_key.private_key_dockercluster.public_key_openssh}"
   filename = "${path.module}/keys/${terraform.workspace}/${lower(random_string.projectId.result)}_openssh.pub"
 }
 
 resource "aws_s3_bucket_object" "uploadPubKey" {
+  count = "${var.aws_key_name == "" ? 1 : 0}"
   bucket     = "${data.terraform_remote_state.baseInfra.s3PubKeyBucket_name}"
   content    = "${tls_private_key.private_key_dockercluster.public_key_openssh}"
   depends_on = ["tls_private_key.private_key_dockercluster"]
@@ -39,11 +46,7 @@ resource "aws_s3_bucket_object" "uploadPubKey" {
   }
 }
 resource "aws_key_pair" "keypairDockercluster" {
-  count   = 1
-  key_name = "${data.template_file.awskeyname.rendered}"
+  count = "${var.aws_key_name == "" ? 1 : 0}"
+  key_name = "${local.resource_prefix}key"
   public_key = "${tls_private_key.private_key_dockercluster.public_key_openssh}"
-}
-
-data "template_file" "awskeyname" {
-  template = "${local.resource_prefix}${terraform.workspace}-dockercluster"
 }
