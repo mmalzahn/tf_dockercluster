ac96f9a3db579a4fd053f45119ebe48cd090b6ca
Matthias Malzahn <matthias.malzahn@comlineag.de>
2018-09-13 19:38:10 +0200

    add dockerMaster in DMZ

--- dockerMaster.tf
+++ dockerMaster.tf
@@ -51,6 +51,47 @@ resource "aws_route53_record" "dockerhost_masterintern" {
   records         = ["${aws_instance.internerDockerhostMaster.private_ip}"]
   zone_id         = "${data.aws_route53_zone.dca_poc_domain.zone_id}"
 }
+resource "aws_instance" "externerDockerhostMaster" {
+  ami           = "${data.aws_ami.dockerhostPackerAmi.id}"
+  instance_type = "${var.dockercluster_instance_type}"
+  subnet_id     = "${element(data.terraform_remote_state.baseInfra.subnet_ids_dmz,0)}"
+
+  vpc_security_group_ids = [
+    "${lookup(data.terraform_remote_state.baseInfra.secgroups,"ssh_bastion_in")}",
+    "${aws_security_group.SG_DockerSocket_IN_from_Bastionhost.id}",
+    "${aws_security_group.SG_DockerSwarmCom_from_dmztiersubnets.id}",
+  ]
+
+  depends_on = [
+    "aws_security_group.SG_DockerSocket_IN_from_Bastionhost",
+    "aws_security_group.SG_DockerSwarmCom_from_dmztiersubnets",
+  ]
+
+  associate_public_ip_address = "true"
+  key_name                    = "${local.resource_prefix}key"
+  user_data                   = "${data.template_file.installscript_master_extern.rendered}"
+
+  lifecycle {
+    ignore_changes        = ["tags.tf_created"]
+    create_before_destroy = "true"
+  }
+
+  tags = "${merge(local.common_tags,
+            map(
+              "Name", "${local.resource_prefix}DockerClusterMasterExtern"
+              )
+              )}"
+}
+
+resource "aws_route53_record" "dockerhost_masterextern" {
+  allow_overwrite = "true"
+  depends_on      = ["aws_instance.externerDockerhostMaster"]
+  name            = "docker0.extern.${terraform.workspace}"
+  ttl             = "60"
+  type            = "A"
+  records         = ["${aws_instance.externerDockerhostMaster.private_ip}"]
+  zone_id         = "${data.aws_route53_zone.dca_poc_domain.zone_id}"
+}
 
 data "template_file" "startSshScript" {
   template = "${file("tpl/start_ssh.tpl")}"
